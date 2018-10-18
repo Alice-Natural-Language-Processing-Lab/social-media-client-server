c93ee1d0a6333f124a22bd300e716cbf5cc52227
Pournami Puthenpurayil Rajan <36324870+PournamiRajan@users.noreply.github.com>
2018-10-17 23:14:40 -0700

    Main function to start the server process

---
+++ socialnetworkserver/src/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 17, 2018
+ *      Author: pournami
+ */
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+int serverInit(string port);
+int acceptConnections(int master_fd);
+
+int main(int argc, char *argv[])
+{
+	string port = "12345";
+	int master_fd, accept_conn;
+
+	switch (argc)
+	{
+	case 1:
+			break;
+	case 2:
+			port = argv[1];
+			break;
+	default:
+			printf("Error: Usage is ./[executable] <port>\n");
+			return -1;
+	}
+	master_fd = serverInit(port);
+	if (master_fd < 0)
+	{
+		printf("Error (serverInit): Server Initialization failed\n");
+		return -1;
+	}
+	accept_conn = acceptConnections(master_fd);
+	if (accept_conn < 0)
+	{
+		printf("Error (acceptConnections): Connection accept error\n");
+		return -1;
+	}
+	return 0;
+}
