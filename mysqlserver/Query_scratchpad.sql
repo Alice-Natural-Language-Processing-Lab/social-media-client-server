79b6965e909dc2bb6225aa167128946bc8217bec
mvonpohle <mvonpohle@gmail.com>
2018-11-03 00:30:38 -0700

    Additional Scratchpad work

--- mysqlserver/Query_scratchpad.sql
+++ mysqlserver/Query_scratchpad.sql
@@ -1,21 +1,3 @@
-select * from social_network.Users;
-select * from social_network.Notifications;
-insert into social_network.Users (userID, userName, passwordHash, salt)
-values (1, 'frodo', 'idorhuckb039', 'xrdoeuch'), (2, 'sam', 'idrhoehu082h3', 'rcicoheur');
-delete from social_network.Users where userID > 0;
-
-CREATE TABLE Notifications (
-	postID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT
-	,userID SMALLINT UNSIGNED NOT NULL
-	,`read` boolean NOT NULL
-	,timestamp DATETIME NOT NULL
-	,PRIMARY KEY (postID)
-	,UNIQUE KEY postID_UNIQUE (postID)
-	,UNIQUE KEY userID_UNIQUE (userID)
-	) engine = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARSET = latin1;
-
-
-
 CREATE TABLE `Users` (
   `userID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `userName` varchar(25) NOT NULL,
@@ -25,3 +7,23 @@ CREATE TABLE `Users` (
   UNIQUE KEY `userID_UNIQUE` (`userID`),
   UNIQUE KEY `userName_UNIQUE` (`userName`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+insert into social_network.Users (userID, userName, passwordHash, salt)
+values (1, 'frodo', 'idorhuckb039', 'xrdoeuch'), (2, 'sam', 'idrhoehu082h3', 'rcicoheur');
+
+CREATE TABLE `Notifications` (
+  `notificationID` int(10) unsigned NOT NULL,
+  `postID` int(10) unsigned NOT NULL,
+  `userID` int(10) unsigned NOT NULL,
+  `read` tinyint(1) unsigned NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`notificationID`),
+  UNIQUE KEY `notificationID_UNIQUE` (`notificationID`),
+  KEY `fk_Notifications_1_idx` (`userID`),
+  CONSTRAINT `fk_Notifications_1` FOREIGN KEY (`userID`) REFERENCES `Users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+select * from social_network.Users;
+select * from social_network.Notifications;
+
+delete from social_network.Users where userID > 0;
