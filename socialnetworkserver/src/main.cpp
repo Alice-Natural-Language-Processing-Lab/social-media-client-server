b441e1bb9aa3041976f6ea73d70ab9d365f5b23f
Pournami Puthenpurayil Rajan <36324870+PournamiRajan@users.noreply.github.com>
2018-11-08 10:58:57 -0800

    Main code. Starting point

--- socialnetworkserver/src/main.cpp
+++ socialnetworkserver/src/main.cpp
@@ -1,8 +1,43 @@
 /*
  * main.cpp
  *
- *  Created on: Nov 8, 2018
- *      Author: michael
+ *  Created on: Oct 17, 2018
+ *      Author: pournami
  */
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include "func_lib.h"
 
+using namespace std;
 
+int main(int argc, char *argv[])
+{
+	string port = "12345";
+	int master_fd, accept_conn;
+
+	switch (argc)
+	{
+	case 1:
+			break;
+	case 2:
+			port = argv[1];
+			break;
+	default:
+			printf("Error: Usage is ./<executable> [port]\n");
+			return -1;
+	}
+	master_fd = serverInit(port);
+	if (master_fd < 0)
+	{
+		printf("Error (serverInit): Server Initialization failed\n");
+		return -1;
+	}
+	accept_conn = acceptConnections(master_fd);
+	if (accept_conn < 0)
+	{
+		printf("Error (acceptConnections): Connection accept error\n");
+		return -1;
+	}
+	return 0;
+}
