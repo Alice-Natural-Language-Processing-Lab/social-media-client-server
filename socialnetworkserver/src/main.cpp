bbd45cc24538c0bfd698ba201d5ebd38fa2720bc
mvonpohle <mvonpohle@gmail.com>
2018-11-08 00:37:39 -0800

    Shell of main working

--- socialnetworkserver/src/main.cpp
+++ socialnetworkserver/src/main.cpp
@@ -7,13 +7,16 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <cstdlib>
 #include "mysql_lib.h"
 
 using namespace std;
 
-int main(){
+int main() {
 
 	int x = 5;
+
+	fprintf(stdout, "%i\n", x);
 
 	exit(EXIT_SUCCESS);
 }
