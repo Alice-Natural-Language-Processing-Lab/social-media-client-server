8ff0a81e5980a95cfab9fcd6d86585a1ef103a0c
mvonpohle <mvonpohle@gmail.com>
2018-11-08 00:06:04 -0800

    Rename to main

---
+++ socialnetworkserver/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 7, 2018
+ *      Author: michael
+ */
+
+#include "mysql_lib.h"
+
+using namespace std;
+
+int main(){
+
+	int x = 5;
+
+	exit(EXIT_SUCCESS);
+}
