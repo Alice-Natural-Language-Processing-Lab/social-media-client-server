ab9bc514f065f2934dc6ffeade9c9c321b32f717
Pournami Puthenpurayil Rajan <36324870+PournamiRajan@users.noreply.github.com>
2018-11-17 23:44:55 -0800

    updated server code to accomodate client changes
    need to add notification thread and DB interfacing

--- socialnetworkserver/src/processClient.cpp
+++ socialnetworkserver/src/processClient.cpp
@@ -4,19 +4,25 @@
  *  Created on: Oct 17, 2018
  *      Author: pournami
  */
+#include <time.h>
 #include "func_lib.h"
 #include "structures.h"
 
 using namespace std;
 
 #define ERR_LEN			256
-#define DEBUG	printf
+#define DEBUG
+
+const char * getCommand(int enumVal)
+{
+  return commandList[enumVal];
+}
 
 void handleClient(int sock_fd);
 void readRequest(int sock_fd, char *buffer, int req_len);
-int parsePacket(struct request *req);
-int sessionValidity(struct request *req);
-int permissionValidity(struct request *req);
+int parsePacket(struct packet *req);
+int sessionValidity(struct packet *req);
+int permissionValidity(struct packet *req, int valid);
 
 /*
  * client initial setup
@@ -29,22 +35,22 @@ int permissionValidity(struct request *req);
 void handleClient(int sock_fd)
 {
 	int sock_read, sock_write, sock_close;
-	struct request *req = (struct request *)malloc(sizeof(struct request));
+	struct packet *req = (struct packet *)malloc(sizeof(struct packet));
 	char *error = (char *)malloc(sizeof(char) * ERR_LEN);
 	int threadId = pthread_self();
 	char *buffer = (char *)req;
 	int req_len, ret;
+	time_t now;
 
 	/* Accept the request persistently*/
 	while(1)
 	{
-		memset(req, 0, sizeof(struct request));
-		req_len = sizeof(struct request);
+		memset(req, 0, sizeof(struct packet));
+		req_len = sizeof(struct packet);
 		/* Read client request */
 		readRequest(sock_fd, buffer, req_len);
-		if (!strcmp(req->command, ""))
+		if(!strlen(buffer))
 			break;
-		DEBUG("Request received: %s\n", req->command);
 		/* Parse the packet for valid packet structure */
 		ret = parsePacket(req);
 		if (ret < 0)
@@ -52,7 +58,9 @@ void handleClient(int sock_fd)
 			printf("Error (parsePacket): Packet parsing/checking failed\n");
 			terminateClient(sock_fd);
 		}
-
+		now = time(NULL);
+		DEBUG("%s: Request received:\n", strtok(ctime(&now), "\n"));
+		DEBUG("%d | %s | %d | %d\n", req->content_len, getCommand(req->cmd_code), req->req_num, req->sessionId);
 		/* Validate session of the client */
 		ret = sessionValidity(req);
 		if (ret < 0) /* session validity could not be established */
@@ -63,24 +71,13 @@ void handleClient(int sock_fd)
 			return;
 		}
 
-		/* Validate permissions of the client */
-		ret = permissionValidity(req);
-		if (ret < 0) /* permission validity could not be established */
-		{
-			printf("Error (permissionValidity): permission validity could not be established\n");
-			/* TODO : SEND WARNING MESSAGE TO CLIENT "NOT LOGGED IN/PERMISSION DENIED" */
-			terminateClient(sock_fd);
-			return;
-		}
-
-		/* process the request for appropriate permissions */
-		ret = processRequest(req);
+		/* process the request with appropriate permissions */
+		ret = processRequest(sock_fd, req);
 		if (ret < 0)
 		{
 			printf("Error (processRequest): request processing failed\n");
 			return;
 		}
-		/* TODO : function call to file where request processing is taken care of*/
 
 		/* TODO : terminate client connection on appropriate request */
 	}
@@ -117,6 +114,7 @@ void readRequest(int sock_fd, char *buffer, int req_len)
 		if (!sock_read || req_len <= 0)
 			break;
 	}
+	cout<<sock_read<<" bytes read\n";
 	return;
 }
 
@@ -125,11 +123,26 @@ void readRequest(int sock_fd, char *buffer, int req_len)
  * req: request structure
  * return 0(Valid Packet) -1(Invalid Packet)
  */
-int parsePacket(struct request *req)
+int parsePacket(struct packet *req)
 {
 	DEBUG("Parsing Packet\n");
 	/* TODO : Complete the function */
-
+	switch (req->cmd_code)
+	{
+	case LOGIN:
+		break;
+	case LOGOUT:
+		break;
+	case POST:
+		break;
+	case LIST:
+		break;
+	case SHOW:
+		break;
+	default:
+		printf("Invalid command, code = %d\n", req->cmd_code);
+		return -1;
+	}
 	return 0;
 }
 
@@ -138,21 +151,38 @@ int parsePacket(struct request *req)
  * req: request structure
  * return 0(Valid session) -1(Invalid session)
  */
-int sessionValidity(struct request *req)
+int sessionValidity(struct packet *req)
 {
 	DEBUG("Checking session validity\n");
 	/* TODO : Complete the function */
-	return 0;
+	int ret = 0;
+	int valid = 1;
+	/* check session validity and modify variable valid*/
+
+	/* Validate permissions of the client */
+	ret = permissionValidity(req, valid);
+	if (ret < 0)
+	{
+		printf("User do not have permission to execute the command\n");
+		return -1;
+	}
+	return ret;
 }
 
 /*
  * permissionValidity() - validate the client session
  * req: request structure
- * return 0(Valid session) -1(Invalid session)
+ * return 0(Valid permission) -1(Invalid permission)
  */
-int permissionValidity(struct request *req)
+int permissionValidity(struct packet *req, int valid)
 {
 	DEBUG("Checking permission for request\n");
 	/* TODO : Complete the function */
+	int ret = 0;
+	if (!valid)
+	{
+		if (req->cmd_code != LOGIN)
+			return -1;
+	}
 	return 0;
 }
