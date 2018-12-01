c7a13ad1ab243b0c29618f703ad728a6ef1b35a7
Pournami Puthenpurayil Rajan <36324870+PournamiRajan@users.noreply.github.com>
2018-12-01 12:14:25 -0800

    updated queries

--- mysqlserver/Query_scratchpad.sql
+++ mysqlserver/Query_scratchpad.sql
@@ -1,19 +1,27 @@
+/*for deleting everything*/
+DROP TABLE `SocialNetwork`.`InteractionLog`;
+DROP TABLE `SocialNetwork`.`Notifications`;
+DROP TABLE `SocialNetwork`.`Posts`;
+DROP TABLE `SocialNetwork`.`Users`;
+
+DROP DATABASE `SocialNetwork`;
+
 CREATE DATABASE `SocialNetwork` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-CREATE TABLE `Users` (
+CREATE TABLE `SocialNetwork`.`Users` (
   `userID` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `userName` varchar(25) NOT NULL,
-  `passwordHash` varchar(45) NOT NULL,
+  `passwordHash` varchar(25) NOT NULL,
   PRIMARY KEY (`userID`),
   UNIQUE KEY `userID_UNIQUE` (`userID`),
   UNIQUE KEY `userName_UNIQUE` (`userName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `Posts` (
+CREATE TABLE `SocialNetwork`.`Posts` (
   `postID` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `posterUserID` smallint(5) unsigned NOT NULL,
   `posteeUserID` smallint(5) unsigned NOT NULL,
-  `timestamp` datetime NOT NULL,
+  `timestamp` datetime(6) NOT NULL DEFAULT NOW(6),
   `content` TEXT NOT NULL,
   PRIMARY KEY (`postID`),
   UNIQUE KEY `postID_UNIQUE` (`postID`),
@@ -23,12 +31,12 @@ CREATE TABLE `Posts` (
   CONSTRAINT `fk_Posts_2` FOREIGN KEY (`posteeUserID`) REFERENCES `Users` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `Notifications` (
+CREATE TABLE `SocialNetwork`.`Notifications` (
   `notificationID` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `postID` smallint(5) unsigned NOT NULL,
   `userID` smallint(5) unsigned NOT NULL,
-  `read` boolean NOT NULL,
-  `timestamp` datetime NOT NULL,
+  `readFlag` boolean NOT NULL DEFAULT 0,
+  `timestamp` datetime(6) NOT NULL DEFAULT NOW(6),
   PRIMARY KEY (`notificationID`),
   UNIQUE KEY `notificationID_UNIQUE` (`notificationID`),
   UNIQUE KEY `post_user_UNIQUE` (`postID`,`userID`),
@@ -38,14 +46,14 @@ CREATE TABLE `Notifications` (
   CONSTRAINT `fk_Notifications_2` FOREIGN KEY (`postID`) REFERENCES `Posts` (`postID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `InteractionLog` (
+CREATE TABLE `SocialNetwork`.`InteractionLog` (
   `interactionID` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `userID` smallint(5) unsigned NOT NULL,
-  `sessionID` varchar(25),
-  `timestamp` datetime NOT NULL,
-  `userStatus` boolean NOT NULL,
-  `socketDescriptor` smallint(5) NOT NULL,
-  `command` varchar(25) NOT NULL,
+  `sessionID` int unsigned,
+  `timestamp` datetime(6) NOT NULL DEFAULT NOW(6),
+  `logout` boolean NOT NULL,
+  `socketDescriptor` smallint(5) unsigned NOT NULL,
+  `command` varchar(50) NOT NULL,
   PRIMARY KEY (`interactionID`),
   UNIQUE KEY `interactionID_UNIQUE` (`interactionID`),
   KEY `fk_InteractionLog_1_idx` (`userID`),
@@ -54,7 +62,7 @@ CREATE TABLE `InteractionLog` (
 
 insert into SocialNetwork.Users (userName, passwordHash)
 values  ('alex', '17663506432727786073'), ('ben', '12927111708687947557'), 
-		    ('cris', '11740314204215096121'), ('don', '12745502948907907845'), 
+		('cris', '11740314204215096121'), ('don', '12745502948907907845'), 
         ('eddy', '4771635686586901585'), ('fred', '3210639344949365877'), 
         ('george', '6136068120051800929'), ('honey', '9050044803222492725'),
         ('imy', '1091350801367770665'), ('jack', '7868285383349367941'),
@@ -63,25 +71,3 @@ values  ('alex', '17663506432727786073'), ('ben', '12927111708687947557'),
         ('omar', '1260992177983512433'), ('pretty', '10940044000550006709'),
         ('quinton', '4523305108125428409'), ('roger', '18264053755285864037'),
         ('sam', '9499914711864451609'), ('tom', '10229820929279828485');
-
-insert into SocialNetwork.Posts (posterUserID, posteeUserID, timestamp, content)
-values (1, 2, '2018-11-07', "Hello friends, how's it going?"), (2, 2, '2018-11-07', "Super political post");
-
-insert into SocialNetwork.Notifications (postID, userID, `read`, timestamp)
-values (1, 1, 0, '2018-11-07 00:00:00'), (2,2,0,'2018-11-07');
-
-insert into SocialNetwork.InteractionLog (userID, sessionID, timestamp, userStatus, socketDescriptor, command)
-values (1, "id2k9h20dx9oeh", '2018-11-07', 1, 5, "show wall frodo");
-
-select * from SocialNetwork.Users;
-select * from SocialNetwork.Posts;
-select * from SocialNetwork.Notifications;
-select * from SocialNetwork.InteractionLog;
-
-/*for deleting everything*/
-DROP TABLE `SocialNetwork`.`InteractionLog`;
-DROP TABLE `SocialNetwork`.`Notifications`;
-DROP TABLE `SocialNetwork`.`Posts`;
-DROP TABLE `SocialNetwork`.`Users`;
-
-DROP DATABASE `SocialNetwork`;
